bfcc50bfd876daa136d84d4a2daf6a5daff2df0a
santiago <santiagopdubov@gmail.com>
2019-02-20 10:56:15 +0000

    working on ir sensor

--- IRLARGE/IRLARGE.ino
+++ IRLARGE/IRLARGE.ino
@@ -7,11 +7,12 @@ void setup() {
 void loop() {
   
   // 5v
-  float volts = analogRead(sensor)*0.0048828125;  // value from sensor * (5/1024)
-  int distance = 13*pow(volts, -1); // worked out from datasheet graph
+  float volts = analogRead(sensor)*0.0048828125*0.5;  // value from sensor * (5/1024)
+  //int distance = 13*pow(volts, -1); // worked out from datasheet graph
+  float distance = 30;
   delay(1000); // slow down serial port 
   
   if (distance <= 30){
-    Serial.println(distance);   // print the distance
+    Serial.println(volts);   // print the distance
   }
 }
