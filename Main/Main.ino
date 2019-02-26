970554583e291d17485f27f42393c6cf09938766
Dan Ley <d.w.ley@hotmail.com>
2019-02-26 11:44:21 +0000

    Turning

--- Main/Main.ino
+++ Main/Main.ino
@@ -1,6 +1,8 @@
 #include <Wire.h>
 #include <Adafruit_MotorShield.h>
 #define button 2
+#define trigPin 13
+#define echoPin 12
 // Create the motor shield object with the default I2C address
 Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
 // Or, create it with a different I2C address (say for stacking)
@@ -10,6 +12,7 @@ Adafruit_MotorShield AFMS = Adafruit_MotorShield();
 Adafruit_DCMotor *L_motor = AFMS.getMotor(1);
 // You can also make another motor on port M2
 Adafruit_DCMotor *R_motor = AFMS.getMotor(2);
+int count = 0;
 
 void setup() {
   // put your setup code here, to run once:
@@ -18,6 +21,8 @@ void setup() {
   Serial.println("Adafruit Motorshield v2 - DC Motor test!");
   AFMS.begin();  // create with the default frequency 1.6KHz
   //AFMS.begin(1000);  // OR with a different frequency, say 1KHz
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
   
 }
 void setMotorSpeed(Adafruit_DCMotor *motor, int motor_speed) {
@@ -37,19 +42,38 @@ void setMotorSpeeds(int L_speed, int R_speed) {
 
 void turn(void){
 // Turn
-  setMotorSpeeds(-255, -100);
-  delay(1500);
-  setMotorSpeeds(150, 255);
-  delay(1000);
-
+  setMotorSpeeds(-255, -50);
+  delay(2000);
 }
 void loop() {
-  setMotorSpeeds(255, 253);
+  /*
+  // Ultrasound
+  double duration, distance;
+  digitalWrite(trigPin, LOW); // Added this line
+  delayMicroseconds(2); // Added this line
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10); // Added this line
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distance = (duration/2) / 29.1;
+  int L = 200+(distance-12)*8;
+  if (L>255) {
+    L = 255;
+  }
+  setMotorSpeeds(L, 200);
   if (digitalRead(button)){
     turn();
   }
-   
-
-  // put your main code here, to run repeatedly:
-
+  Serial.println(distance);
+  */
+  if (count < 3) {
+    setMotorSpeeds(255,250);
+  }
+  else {
+    setMotorSpeeds(255,245);
+  }
+  if (digitalRead(button)){
+    ++count;
+    turn();
+  }
 }
