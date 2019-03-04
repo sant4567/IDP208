b4f69bdbefc3c4526fa6e8523d9d5e3c7f2a2078
Dan Ley <d.w.ley@hotmail.com>
2019-03-04 12:05:45 +0000

    Block Detection

--- IRSMALL/IRSMALL.ino
+++ IRSMALL/IRSMALL.ino
@@ -4,6 +4,16 @@ void setup() {
   pinMode(IRSMALL, INPUT);
 }
 void loop() {
+  if (analogRead(IRSMALL) > 10) {
+    int iter = 0;
+    while (iter < 20 and analogRead(IRSMALL) > 10) {
+      iter+=1;
+      Serial.println("Test");
+    }
+    if (iter>19) {
+      Serial.println("Block Detected");
+    }
+    }
+  }
   Serial.println(analogRead(IRSMALL));
-  delay(300);
 }
