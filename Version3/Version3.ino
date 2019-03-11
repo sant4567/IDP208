5732f751674355ff5261893947c475ac8454cc6a
Dan Ley <d.w.ley@hotmail.com>
2019-03-11 16:59:36 +0000

    shelf_park()

--- Version3/Version3.ino
+++ Version3/Version3.ino
@@ -4,8 +4,8 @@
 #define button 2
 #define side_button 3
 #define IRSMALL A1
-#define gled 6
-#define aled 7
+#define gled 7
+#define aled 6
 //#define amberPin N
 
 // Create the motor shield object with the default I2C address
@@ -18,7 +18,7 @@ Adafruit_DCMotor *S_motor = AFMS.getMotor(3);
 int count = 0;
 int row_count = 0;
 double duration, distance;
-int amber =0;
+int amber =true;
 int iter = 0;
 int hcount=0;
 int reference;
@@ -79,17 +79,17 @@ void turn(void){
   //delay(49000);
   */
   setMotorSpeeds(-200,0);
-  delay(6000);
+  delay(4000);
   setMotorSpeeds(0,200);
   delay(3500);
   setMotorSpeeds(-230,-250);
   delay(3200);
   setMotorSpeeds(190,0);
-  delay(2500);
+  delay(2900);
   setMotorSpeeds(-200,-180);
   delay(2500);
-  setMotorSpeeds(255,220);
-  delay(500);
+  setMotorSpeeds(255,200);
+  delay(800);
 }
 void turn_180(void){
   if (row_count%2) {
@@ -121,46 +121,45 @@ void detectHES(void) {
     setMotorSpeeds(0, 0);
     block_load();
   }
+  digitalWrite(gled,LOW);
 }
 void detectIR(void) {
-  if (analogRead(IRSMALL) > 0) {
-    setMotorSpeeds(0,0);
-    int IR = 0;
-    while (IR < 10 and analogRead(IRSMALL) > 0) {
-      IR+=1;
-    }
-    if (IR>9) {
-      digitalWrite(gled,HIGH);
-      delay(3000);
-      setMotorSpeeds(250, 250);
-      delay(1000);
-      //block_load();
-      //detectHES();
-    }
-  }
+  Serial.println("detectIR");
+//  if (analogRead(IRSMALL) > 0) {
+//    setMotorSpeeds(0,0);
+//    int IR = 0;
+//    while (IR < 10 and analogRead(IRSMALL) > 0) {
+//      IR+=1;
+//    }
+//    if (IR>9) {
+//      digitalWrite(gled,HIGH);
+//      //detectHES();
+//    }
+//  }
 }
 void shelf_park(void) {
   setMotorSpeeds(-150, -145);
   delay(2000);
   setMotorSpeeds(125, 200);
-  delay(2000);
+  delay(1600);
   setMotorSpeeds(200,125);
   delay(2000);
-  /*while (!digitalRead(side_button)){
-    setMotorSpeeds(150, 100);
+  while (!digitalRead(side_button)){
+    setMotorSpeeds(130, 100);
   }
   delay(100);
   while (digitalRead(side_button)){
     setMotorSpeeds(-110,-100);
   }
   setMotorSpeeds(100,100);
-  // activate servo 
-  delay(2000);//driving forwards while cubes slide onto shelf
-  //cubes now deposited on shelf
-  //servo lowers
-  //robot drives forward past shelf towards start zone
+  delay(500);
+  servo.write(50);
+  delay(4000);
+  servo.write(130);
+  delay(1000);
+  // activate servo
   while (!digitalRead(button));
-    setMotorSpeeds(250,250);
+    setMotorSpeeds(200,200);
   //robot reaches final wall next to start zone and begins turning
   setMotorSpeeds(-200,0);
   delay(1000);
@@ -182,6 +181,7 @@ void block_load(void) {
   setSweeperSpeed(100);
   delay(550);
   setSweeperSpeed(0);
+  digitalWrite(gled,LOW);
 }
 void block_reject(void) {
   setMotorSpeeds(100,100);
@@ -258,48 +258,45 @@ void setup() {
   pinMode(IRSMALL, INPUT);
   pinMode(A3, INPUT);
   pinMode(A2, INPUT);
-  pinMode(A0, INPUT);
   pinMode(A1, INPUT);
+  pinMode(A0, INPUT);
   pinMode(10, OUTPUT);
+  pinMode(aled, OUTPUT);
+  pinMode(gled, OUTPUT);
+  digitalWrite(gled,LOW);
   setMotorSpeeds(0, 0);
   setSweeperSpeed(-100);
   delay(2500);
   setSweeperSpeed(100);
   delay(550);
   setSweeperSpeed(0);
-//  servo.attach(servo_pin);
-//  servo.write(150);
-//  delay(1000);
-//  servo.write(140);
-//  delay(2000);
+  servo.attach(servo_pin);
+  servo.write(150);
+  delay(1000);
+  servo.write(130);
+  delay(2000);
 }
 void loop() {
 //  if (hcount==0) {
 //    reference = analogRead(A0);
 //    analogWrite(10, reference/4.01176471);
-//    ++hcount;
+//    hcount++;
 //  }
   setMotorSpeeds(255,240);
   detectIR();
-//  if (iter>500) {
+//  iter++;
+//  if (iter>50) {
 //    iter=0;
 //    amber=not amber;
 //    digitalWrite(aled, amber);
 //  }
   if (digitalRead(button)){
-    if (count<2){
+    if (count<1){
       turn();
-      ++count;
-      Serial.println(count);
     }
-    else if (count>=2) {
-      setMotorSpeeds(-100,-160);
-      delay(2000);
-      setMotorSpeeds(-160,-100);
-      delay(2000);
-      setMotorSpeeds(0,0);
-      delay(1000000);
-      //shelf_park();
+    else if (count>=1) {
+      shelf_park();
     }
+    count++;
   }
 }
